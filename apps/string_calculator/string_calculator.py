b6f6bc0d3d2615f4906f785666438741d1264930
이충엽 <choongyub.lee@29cm.co.kr>
2018-11-16 11:53:34 +0900

    새로운 구분자 개행 추가

--- apps/string_calculator/string_calculator.py
+++ apps/string_calculator/string_calculator.py
@@ -8,6 +8,9 @@ class StringCalculator(object):
         numbers_list = strip_numbers.split(',')
         result = 0
         for number in numbers_list:
-            result += int(number)
+            if number:
+                new_line_number_list = number.split('\n')
+                for new_line_number in new_line_number_list:
+                    result += int(new_line_number)
 
         return result
--- apps/test/test_string_calculator.py
+++ apps/test/test_string_calculator.py
@@ -39,4 +39,42 @@ class TestStringCalculator(object):
         # assert
         assert result == 3
 
+    def test_2개_이상의_숫자를_numbers_인자로받아_합계를_반환한다(self):
+        # arrange
+        numbers = '1,2,3,4,5'
+        # act
+        result = StringCalculator.add(numbers)
+        # assert
+        assert result == 15
 
+    def test_numbers에_구분자_사이에_값이_없어도_정상적으로_합계를_반환한다(self):
+        # arrange
+        numbers = '1,2,3,,4,5'
+        # act
+        result = StringCalculator.add(numbers)
+        # assert
+        assert result == 15
+
+    def test_numbers의_구분자로_개행을_사용해도_정상적으로_합계를_반환한다(self):
+        # arrange
+        numbers = '1\n2\n3'
+        # act
+        result = StringCalculator.add(numbers)
+        # assert
+        assert result == 6
+
+    def test_numbers의_구분자로_개행과_콤마를_모두_사용해도_정상적으로_합계를_반환한다(self):
+        # arrange
+        numbers = '1\n2\n3,4\n5'
+        # act
+        result = StringCalculator.add(numbers)
+        # assert
+        assert result == 15
+
+    def test_numbers가_구분자만_가지고_있는경우_0을_반환합니다(self):
+        # arrange
+        numbers = '\n\n,,\n'
+        # act
+        result = StringCalculator.add(numbers)
+        # assert
+        assert result == 0
